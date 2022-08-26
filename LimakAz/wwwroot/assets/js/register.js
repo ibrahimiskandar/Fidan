e930452a03409b3774f49b3a4eaabcd7ba4d62aa
FidanMemmedova <5TAWDL9@code.edu.az>
2022-08-26 16:08:32 +0400

    user panel courier is deleted

--- LimakAz/wwwroot/assets/js/register.js
+++ LimakAz/wwwroot/assets/js/register.js
@@ -1,15 +1,51 @@
 // Register page Show/Hide Password
 
-const showBtn = Array.from(document.querySelectorAll(".showPass"));
+//const showBtn = Array.from(document.querySelectorAll(".showPass"));
 
-showBtn.forEach(element => {
-    element.addEventListener("click", function ()
-    {
-        if (element.previousElementSibling.type == "password") {
-            element.previousElementSibling.type = "text";
-        } else {
-            element.previousElementSibling.type = "password";
-        }
-    })
-});
+//showBtn.forEach(element => {
+//    element.addEventListener("click", function ()
+//    {
+//        if (element.previousElementSibling.type == "password") {
+//            element.previousElementSibling.type = "text";
+//        } else {
+//            element.previousElementSibling.type = "password";
+//        }
+//    })
+//});
+
+
+const showBtn1 = document.querySelector(".showPassword1");
+const showBtn2 = document.querySelector(".showPassword2");
+
+
+showBtn1.addEventListener("click", function () {
+    if (showBtn1.previousElementSibling.type == "password") {
+        showBtn1.previousElementSibling.type = "text";
+    } else {
+        showBtn1.previousElementSibling.type = "password";
+    }
+})
+showBtn2.addEventListener("click", function () {
+    if (showBtn2.previousElementSibling.type == "password") {
+        showBtn2.previousElementSibling.type = "text";
+    } else {
+        showBtn2.previousElementSibling.type = "password";
+    }
+})
+
+
+//const showBtnTwo = Array.from(document.querySelectorAll(".showPass"));
+
+//showBtnTwo.forEach(element => {
+//    element.addEventListener("click", function () {
+//        if (element.previousElementSibling.type == "password") {
+//            element.previousElementSibling.type = "text";
+//        } else {
+//            element.previousElementSibling.type = "password";
+//        }
+//    })
+//});
+
+
+
 
